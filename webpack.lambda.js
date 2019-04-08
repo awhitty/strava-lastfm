9daec272d25387089f2c52ed930bbaeeea7c3ecd
Austin Whittier <austin.whitt@gmail.com>
2019-04-07 19:55:42 -0700

    no extern

--- webpack.lambda.js
+++ webpack.lambda.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 
 module.exports = {
-  externals: ['express'],
   optimization: { minimize: false },
   plugins: [new webpack.DefinePlugin({ 'global.GENTLY_HIJACK': false })],
 };
